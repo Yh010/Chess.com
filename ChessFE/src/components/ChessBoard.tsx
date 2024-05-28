f167469841ac2a14db1979f3599c5bc56720ac40
Yash Hegde <yash4success@gmail.com>
2024-05-28 16:31:32 +0530

    minor changes

--- ChessFE/src/components/ChessBoard.tsx
+++ ChessFE/src/components/ChessBoard.tsx
@@ -1,28 +1,68 @@
 import { Color, PieceSymbol, Square } from "chess.js";
+import { useState } from "react";
+import { MOVE } from "../pages/GamePage";
 
 function ChessBoard({
+  chess,
   board,
+  socket,
+  setBoard,
 }: {
   board: ({
     square: Square;
     type: PieceSymbol;
     color: Color;
   } | null)[][];
+  socket: WebSocket;
+  setBoard: any;
+  chess: any;
 }) {
+  const [from, setFrom] = useState<null | Square>(null);
+
   return (
     <div className="text-white-200">
       {board.map((row, i) => {
         return (
           <div key={i} className="flex">
             {row.map((square, j) => {
+              const squareRepresentation = (String.fromCharCode(97 + (j % 8)) +
+                "" +
+                (8 - i)) as Square;
               return (
                 <div
+                  onClick={() => {
+                    if (!from) {
+                      setFrom(squareRepresentation);
+                    } else {
+                      socket.send(
+                        JSON.stringify({
+                          type: MOVE,
+                          payload: {
+                            move: {
+                              from,
+                              to: squareRepresentation,
+                            },
+                          },
+                        })
+                      );
+                      setFrom(null);
+                      chess.move({
+                        from,
+                        to: squareRepresentation,
+                      });
+                      setBoard(chess.board());
+                    }
+                  }}
                   key={j}
                   className={`w-16 h-16 ${
                     (i + j) % 2 === 0 ? "bg-green-500" : "bg-green-300"
                   }`}
                 >
-                  {square ? square.type : ""}
+                  <div className="w-full justify-center flex h-full">
+                    <div className="h-full justify-center flex flex-col">
+                      {square ? square.type : ""}
+                    </div>
+                  </div>
                 </div>
               );
             })}
--- ChessFE/src/pages/GamePage.tsx
+++ ChessFE/src/pages/GamePage.tsx
@@ -22,7 +22,6 @@ function GamePage() {
       switch (message.type) {
         case INIT_GAME:
           console.log("game initialized");
-          setChess(new Chess());
           setBoard(chess.board());
           break;
         case MOVE: {
@@ -48,20 +47,27 @@ function GamePage() {
       <div className="pt-8 max-w-screen-lg w-full">
         <div className="grid grid-cols-6 gap-4 w-full bg-red-400">
           <div className="w-full col-span-4 flex justify-center">
-            <ChessBoard board={board} />
+            <ChessBoard
+              chess={chess}
+              setBoard={setBoard}
+              socket={socket}
+              board={board}
+            />
           </div>
-          <div className="col-span-2 w-full bg-green-200">
-            <Button
-              onClick={() => {
-                socket.send(
-                  JSON.stringify({
-                    type: INIT_GAME,
-                  })
-                );
-              }}
-            >
-              Play now
-            </Button>
+          <div className="col-span-2 w-full bg-slate-900 flex justify-center">
+            <div className="pt-8">
+              <Button
+                onClick={() => {
+                  socket.send(
+                    JSON.stringify({
+                      type: INIT_GAME,
+                    })
+                  );
+                }}
+              >
+                Play now
+              </Button>
+            </div>
           </div>
         </div>
       </div>
